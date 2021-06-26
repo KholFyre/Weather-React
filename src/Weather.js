e57d5ce6d875eec5d264c9269e4b904e052e08c5
KholFyre <82007311+KholFyre@users.noreply.github.com>
2021-06-25 22:52:22 -0500

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -3,20 +3,14 @@ import axios from 'axios';
 
 
 
-export default function Weather(props) {
-function handleResponse(response){
-    alert(`the weather in ${response.data.name} is ${response.data.main.temp} C`);
-}
+//export default function Weather(props) {
+//function handleResponse(response){
+  //  alert(`the weather in ${response.data.name} is ${response.data.main.temp} C`);
+//}
 
-let apiKey = "5834061fecd9e62b1d62955b902b96f1";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
-axios.get(apiUrl).then(handleResponse);   
+//let apiKey = "5834061fecd9e62b1d62955b902b96f1";
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
+//axios.get(apiUrl).then(handleResponse);   
 
-return(
-    <h2>Hello From mary poppins
-    
-    </h2>
-
-)
-
-}
+//return(
+  //  <h2>Hello From mary poppins</h2>)}
