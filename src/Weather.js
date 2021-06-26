1b270290fe7fd762c659432b4cd51e9cd348aa49
KholFyre <82007311+KholFyre@users.noreply.github.com>
2021-06-25 23:43:48 -0500

    delete weather

--- src/Weather.js
+++ src/Weather.js
@@ -3,14 +3,3 @@ import axios from 'axios';
 
 
 
-//export default function Weather(props) {
-//function handleResponse(response){
-  //  alert(`the weather in ${response.data.name} is ${response.data.main.temp} C`);
-//}
-
-//let apiKey = "5834061fecd9e62b1d62955b902b96f1";
-//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
-//axios.get(apiUrl).then(handleResponse);   
-
-//return(
-  //  <h2>Hello From mary poppins</h2>)}
